0ede66ab82456a453f2318bf54511934a20f0f88
Ezequiel Miranda <ezequiel.miranda@adevinta.com>
2023-06-18 13:31:27 +0200

    ci: use port 80 for prod

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    server: {
+        port: process.env.NODE_ENV === 'production' ? 80 : 3000,
+      },    
+}
 
 module.exports = nextConfig
