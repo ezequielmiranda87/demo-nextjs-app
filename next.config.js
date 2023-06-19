b3b5ceac56de169da8f4473e9ce6404e12dd5085
Ezequiel Miranda <ezequiel.miranda@adevinta.com>
2023-06-19 10:10:50 +0200

    ci: set-up port

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    server: {
-        port: process.env.NODE_ENV === 'production' ? 80 : 3000,
-      },    
-}
+const nextConfig = {}
 
 module.exports = nextConfig
